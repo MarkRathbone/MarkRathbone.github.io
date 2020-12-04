00c4784b285fba9b9c2a10e2257a12d549172038
Mark Rathbone <MarkAnthonyRathbone@Gmail.com>
2020-12-04 20:10:52 +0000

    Update

--- src/editable-stuff/blog.jsx
+++ src/editable-stuff/blog.jsx
@@ -2,25 +2,41 @@ import { BlogBuilder } from "../components/blog/BlogBuilder";
 const bloglist = [];
 
 const blog = new BlogBuilder({
-  title: "How I learned Terraform",
-  image: "https://i.imgur.com/wl663oz.png",
+  title: "My First Blog",
+  image: "img",
   description:
-    "This is a quick blog post explaining how I managed to teach myself Terraform!",
+    "Description Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, dolores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia provident similique corporis rem ullam odio excepturi voluptas harum, architecto sit!",
 })
-  .addHeading("Why?")
+  .addHeading("Heading")
   .addParagraph(
-    "During AWS re/Start there were somethings we didn't have the time to cover, and although they're very important in reality, they aren't AWS Services so they aren't covered all too much, other than in 'You should know this exists' sense, and James also emphasised the importance of them. Having looked at a few job openings I had also saw a few things popping up over and over again: Terraform and Ansible being two of those."
+    "1st Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
   )
-  .addHeading("Starting Point")
   .addParagraph(
-    "My starting point was simple: Go to the websites. These both had some very nice websites that quite concisely tell you what the tools do but I wanted more, so I went to YouTube. IBM cloud has a number of strong and well presented videos on AWS that I would highly recommend. There were a couple of things I wanted to know coming out of this: why they exist, what they do, and actual use case scenarios."
-  )
-  .addHeading("Hashicorp Configuration Language")
-  .addParagraph("Just like with every other language there was a coding language to learn called HCL (Hashicorp Configuration Language) - which I honestly didn't find too bad. Once again I sat on YouTube and followed along with a video showing how to set up an AWS Infrastructure with a basic web server through Terraform. It was useful - after troubleshooting my mistakes and getting it working, I had a new project I wanted to do. Earlier in the course, when we had first been learning about AWS, I took the time to setup a small Terraria world server in AWS, nothing major or fancy - but something practical and useful for me in the future. I wanted to try setting up that same infrastructure in Terraform now.\n Now that I had the skills to use HCL at a basic level, there were some more things I wanted to find solutions to that weren't covered in the video I watched:\n The video had hard-coded AWS Credentials, which is a major security concern for anything that might be uploaded in the future, it had not used variables whatsoever, and Userdata wouldn't be enough for what I wanted to do, I would also have to learn Ansible and how to deploy it through Terraform."
-  )
-  .addHeading("Initial Setup")
-  .addParagraph("Initially I simply did something very similar to what was used in the video I watched, not using variables or not configuring user data for now and using the Terraform documentation for any AWS Infrastructure that I needed to provision that wasn't covered in the video. I also went through the process in the AWS Console and noted down the steps there and made sure I had it all down in the .tf files before I continued. I then did a Terraform plan run to see if it was all correct, and... Nope. Ofcourse not, but a few typos, and a little bit of problem solving later it was solved. The server of the type I wanted was now being created by Terraform. But before I moved on I wanted to fix issues mentioned earlier. The first was to change the credentials to not be hard-coded, and this was again fixed by using Google to find resources I needed to know to make it use .aws/credentials instead of being coded into the file. Next-up was changing parts of the script to use variables."
+    "2nd Paragraph Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
   );
+
+const blog1 = new BlogBuilder({
+  title: "My Second Blog",
+  image: "img",
+  description:
+    "Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore aut neque consectetur voluptatem quam nihil, facere earum adipisci, animi beatae dicta maiores, ipsam delectus ab molestias dolorum aperiam? Sapiente?",
+})
+  .addHeading("Heading")
+  .addParagraph(
+    "1st Paragraph Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+  )
+  .addParagraph(
+    "2nd Paragraph Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+  )
+  .addHeading("New Heading")
+  .addParagraph(
+    "1st Paragraph Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vero reiciendis quas, autem sit culpa? Quibusdam, cupiditate voluptate a non nulla aliquid enim doloremque ullam, facilis quisquam similique hic omnis!"
+  )
+  .addParagraph(
+    "2nd Paragraph Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur ad, temporibus quae obcaecati eum expedita pariatur aspernatur recusandae beatae iste soluta sunt blanditiis dolore ipsam quia laboriosam quas perspiciatis architecto?"
+  );
+
+bloglist.push(blog1);
 bloglist.push(blog);
 
 export default bloglist;
---
+++ src/editable-stuff/config.js
@@ -0,0 +1,129 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#c9601e, #ad1a30, #1a5cad, #1aad8b",
+  firstName: "Mark",
+  middleName: "Anthony",
+  lastName: "Rathbone",
+  message: "| Bash | Python | Linux | Networking | Security | Cloud ",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/MarkRathbone",
+    },
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/markanthonyrathbone/",
+    },
+    {
+      image: "fa-twitter",
+      url: "https://www.twitter.com/markarathbone/",
+    },
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/markrathbone.jpg"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/16apx8s6zlVwCu7Aea6jdJtXOnAb6GsNP8QAsTv7gWZo/edit",
+
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/markrathbone.jpg"),
+  imageSize: 375,
+  message:
+    "I am a AWS and Microsoft Certified Cloud Practitioner graduate from the AWS/reStart program in Birmingham, UK, by Generation UK&I. I have 10 years of experience working with hardware and I'm now looking for Junior-level work in Cloud. I'm currently very interested in continuing to develop my IAC and Cloud Analytics skills and starting to study about AI and Machine Learning.",
+  resume: require("../editable-stuff/resume.pdf"),
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "Recent Projects",
+  gitHubUsername: "MarkRathbone", //i.e."johnDoe12Gh"
+  reposLength: 4,
+  specificRepos: [],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    { 
+      img: require("../editable-stuff/hashirshoaeb.png"), 
+      label: "First slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+    { 
+      img: require("../editable-stuff/hashirshoaeb.png"), 
+      label: "Second slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+  ],
+  imageSize: {
+    width:"615",
+    height:"450"
+  }
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "Skills",
+  hardSkills: [
+    { name: "Bash", value: 65 },
+    { name: "Python", value: 70 },
+    { name: "Linux", value: 75 },
+    { name: "Networking", value: 60 },
+    { name: "Security", value: 70 },
+    { name: "AWS", value: 90 },
+    { name: "Azure", value: 80 },
+  ],
+  softSkills: [
+    { name: "Goal-Oriented", value: 80 },
+    { name: "Growth Mindset", value: 90 },
+    { name: "Positivity", value: 75 },
+    { name: "Adaptability", value: 85 },
+    { name: "Problem Solving", value: 75 },
+    { name: "Empathy", value: 90 },
+    { name: "Organization", value: 80 },
+    { name: "Collabaration", value: 90 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Get In Touch",
+  message:
+    "I'm currently looking for full-time junior level Cloud Engineering roles! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "MarkAnthonyRathbone@gmail.com",
+};
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export { navBar, mainBody, about, repos, skills, leadership, getInTouch };
